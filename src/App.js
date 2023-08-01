bf11ec01d0511078c64cc242fc3d216962a2843c
EagleRaySinging <kerkertao@gmail.com>
2023-08-01 22:22:34 +0800

    modify App.js import path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import  Register  from "./components/Main/Register/Index.jsx";
-import  Cart  from "components/Main/Cart/Cart.jsx";
+import Register from 'components/Main/Register/Index';
+import Cart from 'components/Main/Cart/Cart';
 import styles from './App.module.css'
 
 function App() {
